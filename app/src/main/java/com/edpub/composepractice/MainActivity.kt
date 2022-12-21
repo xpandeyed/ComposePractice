9c421a4c9d4222bbd886a596006eeece26180139
xpandeyed <lalbiharipandeyg@gmail.com>
2022-12-21 22:33:54 +0530

    hell lot of components added.

--- app/src/main/java/com/edpub/composepractice/MainActivity.kt
+++ app/src/main/java/com/edpub/composepractice/MainActivity.kt
@@ -5,24 +5,24 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Switch
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.edpub.composepractice.ui.theme.ComposePracticeTheme
+import kotlinx.coroutines.launch
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyScreen()
+            ListsFun()
         }
     }
 }
@@ -48,5 +48,67 @@ fun MyScreen(){
             check2.value = it
         } )
     }
+}
 
+@Preview
+@Composable
+fun Preview(){
+    MyScreen2()
+}
+
+@Composable
+fun MyScreen2(){
+
+    val scope = rememberCoroutineScope()
+
+    val scaffoldState = rememberScaffoldState()
+
+    var textField by remember {
+        mutableStateOf("")
+    }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        scaffoldState = scaffoldState
+    ) {
+        Column() {
+            TextField(value = textField, onValueChange = {
+                textField = it
+            },
+            label = {
+                Text("Enter your name")
+            })
+            Button(
+                onClick = {
+                    scope.launch {
+                        scaffoldState.snackbarHostState.showSnackbar("Message on snackbar")
+                    }
+                }
+            ){
+                Text("Press me")
+            }
+        }
+    }
+}
+
+@Composable
+fun ScrollableCol(){
+    //For short scrollable columns
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier.verticalScroll(scrollState)
+    ){
+        for(i in 1..50){
+            Text("Fuck $i")
+        }
+    }
+}
+
+@Composable
+fun ListsFun(){
+    LazyColumn{
+        items(5000){
+            Text("Sexy $it")
+        }
+    }
 }
